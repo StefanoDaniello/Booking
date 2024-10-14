da47be03d6562e8d3614237ba088bce12f5e13fd
StefanoDaniello <stefanodaniello68@gmail.com>
2024-10-14 17:34:01 +0200

    80% layout

--- src/componets/MainComp.tsx
+++ src/componets/MainComp.tsx
@@ -1,14 +1,22 @@
-import { Container } from "@chakra-ui/react";
+import { Container, Box } from "@chakra-ui/react";
 import JumboComp from "./JumboComp";
 import Carusel1 from "./Carusel1";
 import Carusel2 from "./Carusel2";
+import TableTravel from "./TableTravel";
 function MainComp() {
   return (
     <>
       <Container maxW="1400px" p={4}>
         <JumboComp />
-        <Carusel1 />
-        <Carusel2 />
+        <Box my={10}>
+          <Carusel1 />
+          <Carusel2 />
+        </Box>
+        <TableTravel />
+        <Box my={10}>
+          <Carusel1 />
+          <Carusel2 />
+        </Box>
       </Container>
     </>
   );
---
+++ src/componets/TableTravel.tsx
@@ -0,0 +1,122 @@
+import { Box, Text, Container, Grid, GridItem, Image } from "@chakra-ui/react";
+
+function TableTravel() {
+  return (
+    <>
+      <Text fontSize="3xl" fontWeight="bold" mb={4}>
+        Alloggi per le vacanze per ogni tipo di viaggio
+      </Text>
+      <Grid
+        h="400px"
+        templateRows="repeat(4, 1fr)" // Definiamo 4 righe
+        templateColumns="repeat(6, 1fr)" // Simuliamo frazioni con 6 colonne
+        gap={4} // Spaziatura tra gli elementi
+      >
+        {/* Primo container: 1 sopra e 2 sotto */}
+        <GridItem
+          colSpan={2}
+          rowSpan={2}
+          bg="green"
+          borderRadius="10px"
+          overflow="hidden"
+          position="relative"
+          display="flex"
+          justifyContent="center"
+        >
+          <Image
+            src="https://www.piscinecastiglione.it/wp-content/uploads/2023/08/piscina-cascata-casa-design-400x400.jpg?theia_smart_thumbnails_file_version=2"
+            width="full"
+            height="full"
+            objectFit="cover"
+          />
+          <Text
+            position="absolute"
+            fontSize="4xl"
+            fontWeight="bold"
+            bottom={5}
+            color="white"
+            zIndex="10"
+          >
+            Bungalow
+          </Text>
+        </GridItem>
+        {/* Secondo container: 1 container unico */}
+        <GridItem
+          colSpan={2}
+          rowSpan={4}
+          bg="red"
+          borderRadius="10px"
+          overflow="hidden"
+        >
+          <Image
+            src="img/Ville.png"
+            width="full"
+            height="full"
+            objectFit="cover"
+          />
+        </GridItem>
+        {/* Terzo container: 2 sopra e uno sotto */}
+        <GridItem
+          colSpan={2}
+          rowSpan={2}
+          bg="yellow"
+          borderRadius="10px"
+          overflow="hidden"
+        >
+          <Image
+            src="img/Campeggi.png"
+            width="full"
+            height="full"
+            objectFit="cover"
+          />
+        </GridItem>
+
+        {/* Sotto 1 e Sotto 2 occupano metà della larghezza */}
+        <GridItem
+          colSpan={1}
+          rowSpan={2}
+          bg="blue"
+          borderRadius="10px"
+          overflow="hidden"
+        >
+          <Image
+            src="img/Appartamenti.png"
+            width="full"
+            height="full"
+            objectFit="cover"
+          />
+        </GridItem>
+        <GridItem
+          colSpan={1}
+          rowSpan={2}
+          bg="blue"
+          borderRadius="10px"
+          overflow="hidden"
+        >
+          <Image
+            src="img/Chalet.png"
+            width="full"
+            height="full"
+            objectFit="cover"
+          />
+        </GridItem>
+        <GridItem
+          colSpan={2}
+          rowSpan={2}
+          bg="orange"
+          borderRadius="10px"
+          overflow="hidden"
+        >
+          <Image
+            src="img/Case.png"
+            width="full"
+            height="full"
+            objectFit="cover"
+          />
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
+
+export default TableTravel;
